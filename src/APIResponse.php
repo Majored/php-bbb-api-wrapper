625a59c6af46cb031e4b61b322ec53fea2414072
Harry <hello@majored.pw>
2021-12-29 13:06:44 +0000

    Addition of comments & type hints for APIResponse

--- src/APIResponse.php
+++ src/APIResponse.php
@@ -2,26 +2,74 @@
 // Copyright (c) 2021 Harry [Majored] [hello@majored.pw]
 // MIT License (https://github.com/Majored/php-mcm-api-wrapper/blob/main/LICENSE)
 
+/** Represents a parsed response from MC-Market's API. */
 class APIResponse {
+    /** @var string A string representing whether this response was successful or not. */
     private $result;
+
+    /** @var mixed The data of this response if present. */
     private $data;
+
+    /** @var mixed The error of this response if present. */
     private $error;
 
-    function __construct($result, $data, $error) {
+
+    /**
+	 * Constructs a new response from the result, and data or error.
+     * 
+     * @param string The result, either "success" or "error".
+     * @param mixed The data if present.
+     * @param mixed The error if present.
+	 */
+    function __construct(string $result, mixed $data, mixed $error) {
         $this->result = $result;
         $this->data = $data;
         $this->error = $error;
     }
     
-    public function getData() {
+    /**
+	 * Returns whether or not this response was successful.
+     * 
+     * @return bool Whether or not this response was successful.
+	 */
+    function isSuccess(): bool {
+        return $this->result === "success";
+    }
+
+    /**
+	 * Returns the data of this response.
+     * 
+     * @return mixed The response data, or null if it was errored.
+	 */
+    function getData(): mixed {
         return $this->data;
     }
     
-    public function getError() {
+    /**
+	 * Returns whether or not this response was errored.
+     * 
+     * @return bool Whether or not this response was errored.
+	 */
+    function isError(): bool {
+        return $this->result === "error";
+    }
+
+    /**
+	 * Returns the error of this response.
+     * 
+     * @return mixed The response error, or null if it was successful.
+	 */
+    function getError(): mixed {
         return $this->error;
     }
 
-    public static function from_json($json) {
+    /**
+	 * Parse a JSON response and construct a new APIResponse from it.
+     * 
+     * @param string The raw JSON response.
+     * @return APIResponse The newly construct response instance.
+	 */
+    static function from_json(string $json): Self {
         $data = json_decode($json, true);
 
         if (!array_key_exists("data", $data)) {
